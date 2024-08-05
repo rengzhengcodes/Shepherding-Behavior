8aab4026902eca7a3a435cd3fdfc07f0d35ec836
Renggeng Zheng <rzhengcontacts@gmail.com>
2024-08-05 15:13:54 +0200

    l2 fix?

--- test.py
+++ test.py
@@ -140,21 +140,9 @@ def run_morph(rep):
         Target_place_y = np.mean(agents[:, 1])
         # start evolve function
         #! @note L2 is defined in initiate_agent and copied here for brevity.
-        assert agents is not None, "Agent is None"
-        assert shepherd is not None, "Shepherd is None"
-        assert Target_place_x is not None, "Target_place_x is None"
-        assert Target_place_y is not None, "Target_place_y is None"
-        assert 10*(np.sqrt(N_sheep))*2/3 is not None, "L2 is None"
-        assert MODE is not None, "MODE is None"
-        # print(agents, type(agents))
-        # print(shepherd, type(shepherd))
-        # print(Target_place_x, type(Target_place_x))
-        # print(Target_place_y, type(Target_place_y))
-        # print("L2: ", type(10*(np.sqrt(N_sheep))*2/3))
-        # print(MODE, type(MODE))
-        
+        L2 = 10*(np.sqrt(N_sheep))*2/3
         agents_update, shepherd_update, max_agents_indexes = evolve(agents, shepherd, Target_place_x, 
-                                                                    Target_place_y, L2 := 10*(np.sqrt(N_sheep))*2/3, MODE=MODE)
+                                                                    Target_place_y, L2, MODE=MODE)
         # update data
         agents = agents_update
         shepherd = shepherd_update
