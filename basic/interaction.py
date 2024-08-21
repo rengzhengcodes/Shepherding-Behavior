cb3ae54ef5312326a86ccd4ab3d906d2df153cab
Renggeng Zheng <rzhengcontacts@gmail.com>
2024-08-22 01:06:59 +0200

    hope it's happy now:

--- basic/interaction.py
+++ basic/interaction.py
@@ -205,9 +205,10 @@ def get_fence_force(agents: np.ndarray, shepherds: np.ndarray,
     unaffected_agents: np.ndarray = agents[:, 21] == 1 or (
         FENCE_MIDDLE_ANGLE - GATE_ANGULAR_WIDTH / 2 <= agent_angle <= FENCE_MIDDLE_ANGLE + GATE_ANGULAR_WIDTH / 2
     ) # agents in the target or in the gate
+    fence_range_agents: np.ndarray = agent_dist <= t_r + agents[:, 7]
+    affected_agents: np.ndarray = np.logical_not(unaffected_agents) and fence_range_agents
     f_fence_on_sheep: np.ndarray = np.where(
-        not unaffected_agents and agent_dist <= t_r + agents[:, 7],
-        fence - agents[:, :2], np.zeros((agents.shape[0], 2))
+        affected_agents, fence - agents[:, :2], np.zeros((agents.shape[0], 2))
     )
 
     # Calculates the repulsion force between the shepherds and the fence.
