682a6404bab7cfaa6cdff315c411cf8951ccc1e1
Renggeng Zheng <rzhengcontacts@gmail.com>
2024-08-22 01:20:27 +0200

    same change for shepherds

--- basic/interaction.py
+++ basic/interaction.py
@@ -208,16 +208,21 @@ def get_fence_force(agents: np.ndarray, shepherds: np.ndarray,
     fence_range_agents: np.ndarray = agent_dist <= t_r + agents[:, 7]
     affected_agents: np.ndarray = np.logical_not(unaffected_agents) and fence_range_agents
     # Casts the affected agents to a 2D array.
-    affected_agents: np.ndarray = np.expand_dims(affected_agents, axis=1)
+    # affected_agents: np.ndarray = np.expand_dims(affected_agents, axis=1)
     f_fence_on_sheep: np.ndarray = np.where(
         affected_agents, fence - agents[:, :2], np.zeros((agents.shape[0], 2))
     )
 
     # Calculates the repulsion force between the shepherds and the fence.
+    unaffected_shepherds: np.ndarray = np.logical_not(
+        FENCE_MIDDLE_ANGLE - GATE_ANGULAR_WIDTH / 2 <= shepherd_angle <= FENCE_MIDDLE_ANGLE + GATE_ANGULAR_WIDTH / 2
+    )
+    fence_range_shepherds: np.ndarray = shepherd_dist <= t_r + shepherds[:, 7]
+    affected_shepherds: np.ndarray = np.logical_not(unaffected_shepherds) and fence_range_shepherds
+    # Casts the affected shepherds to a 2D array.
+    # affected_shepherds: np.ndarray = np.expand_dims(affected_shepherds, axis=1)
     f_fence_on_shepherds = np.where(
-        shepherd_dist <= t_r + shepherds[:, 7] and not (
-            FENCE_MIDDLE_ANGLE - GATE_ANGULAR_WIDTH / 2 <= shepherd_angle <= FENCE_MIDDLE_ANGLE + GATE_ANGULAR_WIDTH / 2
-        ), 
+        affected_shepherds, 
         fence - shepherds[:, :2], 
         np.zeros(shepherds.shape[0], 2)
     )
